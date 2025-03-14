1499d120c4a4edd11d50fb667c817a684a21628d
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-03-14 14:00:51 -0400

    Filter out transcriptions with no play-by-play commentaries

--- scripts/gen_anns_soccernet.py
+++ scripts/gen_anns_soccernet.py
@@ -11,15 +11,17 @@ def main(pbp_annotated_dir: str, output_file: str) -> None:
     for pbp_annotated_path in Path(pbp_annotated_dir).glob("**/*.json"):
         with open(pbp_annotated_path) as f:
             pbp_annotated = json.load(f)
-            utters = convert_pbp_annotated(pbp_annotated)
-            anns[
-                "/".join(
-                    [
-                        pbp_annotated_path.parts[-2],
-                        pbp_annotated_path.stem.replace("_annotated", ""),
-                    ]
-                )
-            ] = utters
+        utters = convert_pbp_annotated(pbp_annotated)
+        if len(utters) == 0:
+            continue
+        anns[
+            "/".join(
+                [
+                    pbp_annotated_path.parts[-2],
+                    pbp_annotated_path.stem.replace("_annotated", ""),
+                ]
+            )
+        ] = utters
     with open(output_file, "w") as f:
         json.dump(anns, f, indent=4)
 
