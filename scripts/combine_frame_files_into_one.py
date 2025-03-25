e70ec29afb07e681b8ba4e66b7a3d8355c3b1a25
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-03-24 21:10:16 -0400

    Use the real time dataset to figure out the video IDs

--- scripts/combine_frame_files_into_one.py
+++ scripts/combine_frame_files_into_one.py
@@ -6,13 +6,15 @@ import torch
 from jsonargparse import auto_cli
 from tqdm import tqdm
 
+from real_time_vlm_benchmark.datasets import RealTimeDataset
+
 
 def combine(
-    encoded_frames_dir_path: Path, output_dir_path: Path, video_dir: Path
+    encoded_frames_dir_path: Path, output_dir_path: Path, video_id: str
 ) -> None:
     frame_idx = []
     frame_embeds_list = []
-    video_id = str(video_dir.relative_to(encoded_frames_dir_path))
+    video_dir = encoded_frames_dir_path / video_id
     for frame_file in video_dir.iterdir():
         frame_idx.append(int(frame_file.stem))
         frame_embeds_list.append(torch.load(frame_file))
@@ -23,30 +25,27 @@ def combine(
     torch.save(frames_dict, output_path)
 
 
-def main(encoded_frames_dir: str, output_dir: str) -> None:
+def main(
+    real_time_dataset: RealTimeDataset, encoded_frames_dir: str, output_dir: str
+) -> None:
     encoded_frames_dir_path = Path(encoded_frames_dir)
-    video_dirs = [
-        p
-        for p in tqdm(encoded_frames_dir_path.glob("**/*"), desc="Finding video dirs")
-        if not p.name.endswith(".pt")
-    ]
+    video_ids = set(d["video_id"] for d in iter(real_time_dataset))
     output_dir_path = Path(output_dir)
     combine_fn = partial(combine, encoded_frames_dir_path, output_dir_path)
     with ThreadPoolExecutor() as executor:
-        future_to_video_dir = {
-            executor.submit(combine_fn, video_dir): video_dir
-            for video_dir in video_dirs
+        future_to_video_id = {
+            executor.submit(combine_fn, video_id): video_id for video_id in video_ids
         }
         for future in tqdm(
-            as_completed(future_to_video_dir),
-            total=len(future_to_video_dir),
+            as_completed(future_to_video_id),
+            total=len(future_to_video_id),
             desc="Combining",
         ):
-            video_dir = future_to_video_dir[future]
+            video_id = future_to_video_id[future]
             try:
                 future.result()
             except Exception as e:
-                print(f"Exception for {video_dir}: {e}")
+                print(f"Exception for {video_id}: {e}")
 
 
 if __name__ == "__main__":
