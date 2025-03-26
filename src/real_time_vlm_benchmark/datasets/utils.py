f8232f6184198067b2aa1ba33535782a35e5c27a
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-03-26 02:36:27 -0400

    Revert "Use cv2 to get avg fps"
    This reverts commit b4ca005949d03c0ad7de9a0204f8b92dea5d1e68.

--- src/real_time_vlm_benchmark/datasets/utils.py
+++ src/real_time_vlm_benchmark/datasets/utils.py
@@ -1,9 +1,13 @@
 from collections import defaultdict
-from pathlib import Path
 
-import cv2
 import torch
 from torch.utils.data import Dataset
+
+# decord must be imported after torch
+# https://github.com/dmlc/decord/issues/293
+from decord import VideoReader  # isort: skip
+from pathlib import Path
+
 from tqdm import tqdm
 
 from real_time_vlm_benchmark.baseline_models.utils.sample import (
@@ -21,14 +25,11 @@ def convert_to_frame_dataset(
     for datapoint in tqdm(
         real_time_dataset, disable=not show_progress, desc="Convert to Frame Dataset"
     ):
+        vr = VideoReader(str(datapoint["video_path"]))
         dialogue = datapoint["dialogue"]
 
-        vidcap = cv2.VideoCapture(str(datapoint["video_path"]))
         frame_idx, _, _ = sample_frames_for_dialogue(
-            dialogue,
-            vidcap.get(cv2.CAP_PROP_FPS),
-            sample_fps,
-            max_num_frames=max_num_frames,
+            dialogue, vr.get_avg_fps(), sample_fps, max_num_frames=max_num_frames
         )
         for frame_id in frame_idx.tolist():
             frame_data[(datapoint["video_id"], datapoint["video_path"])].add(frame_id)
