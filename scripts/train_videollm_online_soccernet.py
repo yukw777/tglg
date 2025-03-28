12d4a76ccda6668a32087863cbe46c6c355c8fad
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-03-28 01:45:35 -0400

    Set metric_for_best_model to eval_loss

--- scripts/train_videollm_online_soccernet.py
+++ scripts/train_videollm_online_soccernet.py
@@ -104,7 +104,7 @@ def train() -> None:
 
     # Load the best model at the end so we can save it
     videollm_online_args.load_best_model_at_end = True
-    videollm_online_args.metric_for_best_model = "loss"
+    videollm_online_args.metric_for_best_model = "eval_loss"
     # Workaround for https://github.com/huggingface/transformers/issues/26969
     # and https://github.com/huggingface/transformers/issues/23018
     videollm_online_args.gradient_checkpointing_kwargs = {"use_reentrant": False}
