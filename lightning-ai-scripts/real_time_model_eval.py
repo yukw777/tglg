abcf8b01c3a35d5089735abe358f9aa6fe57be8e
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-03-31 21:04:46 -0400

    Add a lightning ai batch job script for real-time model evaluation

---
+++ lightning-ai-scripts/real_time_model_eval.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+from jsonargparse import auto_cli
+from lightning_sdk import Job, Machine, Studio
+
+
+def main(
+    studio_user: str,
+    studio_name: str,
+    teamspace: str,
+    root_results_dir: Path,
+    log_dir: Path,
+    run_name: str,
+    cwd: str = ".",
+    run_inference_args: dict[str, str] | None = None,
+    dry_run: bool = False,
+    machine: str = "L40S_X_4",
+    interruptible: bool = True,
+) -> None:
+    torchrun_map = {
+        Machine.L40S: "--nnodes=1 --nproc_per_node=1",
+        Machine.L40S_X_4: "--nnodes=1 --nproc_per_node=4",
+    }
+    selected_machine = getattr(Machine, machine)
+    assert selected_machine in torchrun_map, f"{machine} is not supported."
+
+    if not dry_run:
+        studio = Studio(user=studio_user, name=studio_name, teamspace=teamspace)
+        studio.start()
+
+    script_cmd = f"cd {cwd} && torchrun {torchrun_map[selected_machine]} --tee 3 --log-dir {log_dir / run_name} scripts/run_inference.py \\"
+    script_args = {
+        # NOTE: this has to come before any other "model" args
+        "model": "real_time_vlm_benchmark.baseline_models.videollm_online_models.RealTimeSoccerNetModel"
+    }
+    if run_inference_args is None:
+        run_inference_args = {}
+
+    run_inference_args["results_dir"] = str(root_results_dir / run_name)
+    run_inference_args["wandb_run_name"] = run_name
+    script_args = {**script_args, **run_inference_args}
+    script_args_str = " \\\n".join(f"--{k} {v}" for k, v in script_args.items())
+    cmd = "\n".join([script_cmd, script_args_str])
+    print(cmd)
+    print()
+    if not dry_run:
+        Job.run(
+            command=cmd,
+            name=run_name,
+            machine=selected_machine,
+            studio=studio,
+            interruptible=interruptible,
+        )
+
+
+if __name__ == "__main__":
+    auto_cli(main, as_positional=False)
