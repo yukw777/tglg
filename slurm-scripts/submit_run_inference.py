b1c69d1691c01401b4ebc5581e9c41bf99218c7b
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-04-05 21:42:02 -0400

    Fix submit_run_inference.py
    Fixes related to c7f372bc2f381568d008d1752188310492c5efcd

--- slurm-scripts/submit_run_inference.py
+++ slurm-scripts/submit_run_inference.py
@@ -8,7 +8,8 @@ def main(
     account: str,
     partition: str,
     time: str,
-    num_gpus: int,
+    total_gpus: int,
+    gpus_per_node: int,
     mem_per_gpu: str,
     num_dataloader_workers: int,
     email: str | None = None,
@@ -16,6 +17,14 @@ def main(
     dry_run: bool = False,
     run_inference_args: dict[str, str] | None = None,
 ) -> None:
+    # Validate that total_gpus is evenly divisible by gpus_per_node.
+    if total_gpus % gpus_per_node != 0:
+        raise ValueError(
+            f"Total GPUs ({total_gpus}) must be evenly divisible by GPUs per node ({gpus_per_node})."
+        )
+    # Compute the number of nodes required.
+    nodes = total_gpus // gpus_per_node
+
     if email is None:
         email = ""
     else:
@@ -26,13 +35,14 @@ def main(
         hf_home = f"export HF_HOME={hf_home}"
 
     multi_gpu = f"""RDZV_ID=$RANDOM
-MASTER_NODE=$(srun --nodes=1 --ntasks=1 hostname)
-srun --cpus-per-task {num_dataloader_workers} uv run torchrun \\
---nnodes={num_gpus} \\
---nproc_per_node=1 \\
+MASTER_NODE=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
+MASTER_PORT=$((1024 + RANDOM % 64512))
+srun uv run torchrun \\
+--nnodes={nodes} \\
+--nproc_per_node={gpus_per_node} \\
 --rdzv-id=$RDZV_ID \\
 --rdzv-backend=c10d \\
---rdzv-endpoint=$MASTER_NODE \\
+--rdzv-endpoint=$MASTER_NODE:$MASTER_PORT \\
 scripts/run_inference.py \\"""
     single_gpu = "uv run python scripts/run_inference.py \\"
 
@@ -43,12 +53,12 @@ scripts/run_inference.py \\"""
 
 #SBATCH --partition={partition}
 #SBATCH --time={time}
-#SBATCH --job-name=run-inference-{job_name}
+#SBATCH --job-name=run-inference:{job_name}
 {email}
 #SBATCH --account={account}
-#SBATCH --ntasks={num_gpus}
-#SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task={num_dataloader_workers}
+#SBATCH --nodes={nodes}
+#SBATCH --gpus-per-node={gpus_per_node}
+#SBATCH --cpus-per-task={num_dataloader_workers * gpus_per_node}
 #SBATCH --mem-per-gpu={mem_per_gpu}
 #SBATCH --output=%x-%j.log
 
@@ -57,7 +67,7 @@ module load cuda gcc
 export DECORD_EOF_RETRY_MAX=20480
 {hf_home}
 export WANDB_NAME={job_name}
-{single_gpu if num_gpus < 2 else multi_gpu}
+{single_gpu if total_gpus < 2 else multi_gpu}
 --num_dataloader_workers {num_dataloader_workers} \
 {args}
 """
