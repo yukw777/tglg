5d52cd9c91a4818bceb6910a627a305cb302811a
Peter Yu <2057325+yukw777@users.noreply.github.com>
2025-04-06 20:22:50 -0400

    Set mp_manager arguments only if multi-gpu

--- slurm-scripts/submit_run_inference.py
+++ slurm-scripts/submit_run_inference.py
@@ -55,8 +55,9 @@ scripts/run_inference.py \\"""
     run_inference_args["num_dataloader_workers"] = str(num_dataloader_workers)
     run_inference_args["wandb_project"] = wandb_project
     run_inference_args["wandb_run_name"] = job_name
-    run_inference_args["mp_manager_ip_addr"] = "$MASTER_NODE"
-    run_inference_args["mp_manager_port"] = "$MP_MANAGER_PORT"
+    if total_gpus > 1:
+        run_inference_args["mp_manager_ip_addr"] = "$MASTER_NODE"
+        run_inference_args["mp_manager_port"] = "$MP_MANAGER_PORT"
     run_inference_args["results_dir"] = str(results_dir_root / job_name)
     args = " \\\n".join(f"--{k} {v}" for k, v in run_inference_args.items())
     script = rf"""#!/bin/bash
